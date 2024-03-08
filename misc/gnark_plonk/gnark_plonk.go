522b02ca07dc9876746305a465dbe9fe6c9c2da7
fborello-lambda <federicoborello@lambdaclass.com>
2024-03-08 15:38:22 -0300

    Add de/serialization WIP

--- misc/gnark_plonk/gnark_plonk.go
+++ misc/gnark_plonk/gnark_plonk.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
+	"os"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/plonk"
@@ -75,7 +78,66 @@ func main() {
 
 	proof, _ := plonk.Prove(ccs, pk, witnessFull)
 
-	err := plonk.Verify(proof, vk, witnessPublic)
+	// Serialize
+	var pk_buf bytes.Buffer
+	pk.WriteTo(&pk_buf)
+	pk_buf_base64 := make([]byte, base64.StdEncoding.EncodedLen(pk_buf.Len()))
+	base64.StdEncoding.Encode(pk_buf_base64, pk_buf.Bytes())
+	os.WriteFile("pk.bin", pk_buf_base64, 0644)
+	fmt.Printf("Done")
+
+	var vk_buf bytes.Buffer
+	vk.WriteTo(&vk_buf)
+	vk_buf_base64 := make([]byte, base64.StdEncoding.EncodedLen(vk_buf.Len()))
+	base64.StdEncoding.Encode(vk_buf_base64, vk_buf.Bytes())
+	os.WriteFile("vk.bin", vk_buf_base64, 0644)
+	fmt.Printf("Done")
+
+	var proof_buf bytes.Buffer
+	proof.WriteTo(&proof_buf)
+	proof_buf_base64 := make([]byte, base64.StdEncoding.EncodedLen(proof_buf.Len()))
+	base64.StdEncoding.Encode(proof_buf_base64, vk_buf.Bytes())
+	os.WriteFile("proof.bin", proof_buf_base64, 0644)
+	fmt.Printf("Done")
+
+	var publicWitness_buf bytes.Buffer
+	witnessPublic.WriteTo(&publicWitness_buf)
+	publicWitness_buf_base64 := make([]byte, base64.StdEncoding.EncodedLen(publicWitness_buf.Len()))
+	base64.StdEncoding.Encode(publicWitness_buf_base64, publicWitness_buf.Bytes())
+	os.WriteFile("pw.bin", publicWitness_buf_base64, 0644)
+	fmt.Printf("Done")
+
+	// Deserialize Proof
+	proof_des := plonk.NewProof(ecc.BN254)
+	proof_base64, _ := os.ReadFile("proof.bin")
+
+	proof_bytes := make([]byte, base64.StdEncoding.DecodedLen(len(proof_base64)))
+	base64.StdEncoding.Decode(proof_bytes, proof_base64)
+
+	proof_reader := bytes.NewReader(proof_bytes)
+	proof_des.ReadFrom(proof_reader)
+	fmt.Println("DES")
+	// Deserialize VK
+	vk_des := plonk.NewVerifyingKey(ecc.BN254)
+	vk_base64, _ := os.ReadFile("vk.bin")
+
+	vk_bytes := make([]byte, base64.StdEncoding.DecodedLen(len(vk_base64)))
+	base64.StdEncoding.Decode(vk_bytes, vk_base64)
+
+	vk_reader := bytes.NewReader(vk_bytes)
+	vk_des.ReadFrom(vk_reader)
+	fmt.Println("DES")
+	// Deserialize PublicWitness
+	pw_des, _ := frontend.NewWitness(&w, ecc.BN254.ScalarField(), frontend.PublicOnly())
+	pw_base64, _ := os.ReadFile("pw.bin")
+
+	pw_bytes := make([]byte, base64.StdEncoding.DecodedLen(len(pw_base64)))
+	base64.StdEncoding.Decode(pw_bytes, pw_base64)
+
+	pw_reader := bytes.NewReader(pw_bytes)
+	pw_des.ReadFrom(pw_reader)
+	fmt.Println("DES")
+	err := plonk.Verify(proof_des, vk_des, witnessPublic)
 
 	if err != nil {
 		fmt.Print("Invalid Proof")

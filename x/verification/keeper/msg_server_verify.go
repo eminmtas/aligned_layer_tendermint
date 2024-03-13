be18cada8478da0ff26e816e59cad1ae11ddef7f
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-12 23:16:58 -0300

    Remove unused debug code

--- x/verification/keeper/msg_server_verify.go
+++ x/verification/keeper/msg_server_verify.go
@@ -4,21 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
-	"fmt"
 	"io"
 	"strconv"
 
 	"alignedlayer/x/verification/types"
 
 	"github.com/consensys/gnark-crypto/ecc"
-	"github.com/consensys/gnark-crypto/kzg"
 	"github.com/consensys/gnark/backend/plonk"
 	"github.com/consensys/gnark/backend/witness"
-	"github.com/consensys/gnark/constraint"
-	cs "github.com/consensys/gnark/constraint/bn254"
-	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/frontend/cs/scs"
-	"github.com/consensys/gnark/test"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -32,56 +25,6 @@ func (k msgServer) Verify(goCtx context.Context, msg *types.MsgVerify) (*types.M
 	ctx.EventManager().EmitEvent(event)
 
 	return &types.MsgVerifyResponse{}, nil
-}
-
-type Circuit struct {
-	X frontend.Variable `gnark:"x"`       // x  --> secret visibility (default)
-	Y frontend.Variable `gnark:",public"` // Y  --> public visibility
-}
-
-// Define declares the circuit logic. The compiler then produces a list of constraints
-// which must be satisfied (valid witness) in order to create a valid zk-SNARK
-func (circuit *Circuit) Define(api frontend.API) error {
-	// compute x**3 and store it in the local variable x3.
-	x3 := api.Mul(circuit.X, circuit.X, circuit.X)
-
-	// compute x**3 + x + 5 and store it in the local variable res
-	res := api.Add(x3, circuit.X, 5)
-
-	// assert that the statement x**3 + x + 5 == y is true.
-	api.AssertIsEqual(circuit.Y, res)
-	return nil
-}
-
-// Defines the proof that will be proved.
-func toProve() Circuit {
-	return Circuit{
-		X: 3,
-		Y: 35,
-	}
-}
-
-func get_proof() (plonk.Proof, plonk.VerifyingKey, constraint.ConstraintSystem, kzg.SRS) {
-	var myCircuit Circuit
-	ccs, _ := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, &myCircuit)
-
-	scs := ccs.(*cs.SparseR1CS)
-	kzgsrs, _ := test.NewKZGSRS(scs)
-
-	fmt.Println(scs.Coefficients)
-
-	assignment := toProve()
-
-	fullWitness, _ := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
-
-	pk, vk, _ := plonk.Setup(ccs, kzgsrs)
-
-	var vk_buffer bytes.Buffer
-	vk.WriteTo(&vk_buffer)
-
-	proof, _ := plonk.Prove(ccs, pk, fullWitness)
-
-	return proof, vk, ccs, kzgsrs
 }
 
 func verify(msg *types.MsgVerify) bool {

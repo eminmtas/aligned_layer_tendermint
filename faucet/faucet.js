7d1bd8ee3faff1ff74fc38a6d5729f003403da16
fborello-lambda <federicoborello@lambdaclass.com>
2024-03-13 13:34:07 -0300

    "Add changes"

--- faucet/faucet.js
+++ faucet/faucet.js
@@ -68,7 +68,7 @@ app.get('/send/:chain/:address', async (req, res) => {
         if (await checker.checkAddress(address, chain) && await checker.checkIp(`${chain}${ip}`, chain)) {
           checker.update(`${chain}${ip}`) // get ::1 on localhost
           console.log('send tokens to ', address)
-          sendTx(address, chain).then(ret => {
+          await sendTx(address, chain).then(ret => {
             console.log(ret)
             checker.update(address)
             res.send({ result: ret })

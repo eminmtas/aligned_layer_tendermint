6bedc3c581d7f631f5420f7cd204c27c4a993262
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2024-03-20 18:51:14 -0300

    Add mutex to faucet and set error status codes on requests tokens responses (#76)
    * Add mutex to faucet
    
    * Remove mutex's timeout
    
    * Set error codes in send responses
    
    * Return 500 if mutex is locked
    
    * Remove unused import
    
    * Refactor code
    
    * Refactor code
    
    * Add timeout to mutex
    
    With this, the requests are not rejected instantly if the mutex is locked.
    After some testing, this results in a much better ratio of successful requests
    
    ---------
    
    Co-authored-by: JuArce <52429267+JuArce@users.noreply.github.com>
    Co-authored-by: Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
    Co-authored-by: Federico Borello <156438142+fborello-lambda@users.noreply.github.com>

--- faucet/faucet.js
+++ faucet/faucet.js
@@ -6,9 +6,12 @@ import { SigningStargateClient } from "@cosmjs/stargate";
 import conf from './config/config.js'
 import { FrequencyChecker } from './checker.js';
 
+import { Mutex, withTimeout, E_TIMEOUT } from 'async-mutex';
 
 // load config
 console.log("loaded config: ", conf)
+
+const mutex = withTimeout(new Mutex(), 10000);
 
 const app = express()
 
@@ -61,34 +64,43 @@ app.get('/send/:chain/:address', async (req, res) => {
   const { chain, address } = req.params;
   const ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['X-Forwarded-For'] || req.ip
   console.log('request tokens from', address, ip)
-  if (chain || address) {
-    try {
-      const chainConf = conf.blockchains.find(x => x.name === chain)
-      if (chainConf && address.startsWith(chainConf.sender.option.prefix)) {
-        if (await checker.checkAddress(address, chain) && await checker.checkIp(`${chain}${ip}`, chain)) {
-          checker.update(`${chain}${ip}`) // get ::1 on localhost
-          console.log('send tokens to ', address)
-          await sendTx(address, chain).then(ret => {
-            console.log(ret)
-            checker.update(address)
-            res.send({ result: {code: ret.code, tx_hash: ret.transactionHash, height: ret.height} })
-          }).catch(err => {
-            res.send({ result: `err: ${err}` })
-          });
-        } else {
-          res.send({ result: "You requested too often" })
-        }
-      } else {
-        res.send({ result: `Address [${address}] is not supported.` })
-      }
-    } catch (err) {
-      console.error(err);
-      res.send({ result: 'Failed, Please contact to admin.' })
+
+  if (!chain && !address) {
+    res.send({ result: 'address is required' })
+    return
+  }
+
+  try {
+    const chainConf = conf.blockchains.find(x => x.name === chain)
+    if (!chainConf || !address.startsWith(chainConf.sender.option.prefix)) {
+      res.status(400).send({ result: `Address [${address}] is not supported.` })
+      return
     }
 
-  } else {
-    // send result
-    res.send({ result: 'address is required' });
+    if (!await checker.checkAddress(address, chain) || !await checker.checkIp(`${chain}${ip}`, chain)) {
+      res.status(429).send({ result: "You requested too often" })
+      return
+    }
+
+    checker.update(`${chain}${ip}`) // get ::1 on localhost
+    console.log('send tokens to ', address)
+
+    await mutex.runExclusive(async () => {
+      await sendTx(address, chain).then(ret => {
+        console.log(ret)
+        checker.update(address)
+        res.send({ result: { code: ret.code, tx_hash: ret.transactionHash, height: ret.height } })
+      }).catch(err => {
+        res.status(500).send({ result: `err: ${err}` })
+      });
+    });
+  } catch (err) {
+    console.error(err);
+
+    if (err == E_TIMEOUT) {
+      return res.status(500).send({ result: 'Faucet is busy, Please try again later.' })
+    }
+    res.status(500).send({ result: 'Failed, Please contact to admin.' })
   }
 })
 
@@ -98,21 +110,22 @@ app.listen(conf.port, () => {
 
 async function sendTx(recipient, chain) {
   const chainConf = conf.blockchains.find(x => x.name === chain)
-  if (chainConf) {
-    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
-    const [firstAccount] = await wallet.getAccounts();
-    console.log("sender", firstAccount);
-
-    const rpcEndpoint = chainConf.endpoint.rpc_endpoint;
-    const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
-
-    const amount = chainConf.tx.amount;
-    const fee = chainConf.tx.fee;
-    console.log("recipient", recipient, amount, fee);
-
-    let response = client.sendTokens(firstAccount.address, recipient, amount, fee)
-    console.log(response)
-    return response;
+  if (!chainConf) {
+    throw new Error(`Blockchain Config [${chain}] not found`)
   }
-  throw new Error(`Blockchain Config [${chain}] not found`)
+
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
+  const [firstAccount] = await wallet.getAccounts();
+  console.log("sender", firstAccount);
+
+  const rpcEndpoint = chainConf.endpoint.rpc_endpoint;
+  const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
+
+  const amount = chainConf.tx.amount;
+  const fee = chainConf.tx.fee;
+  console.log("recipient", recipient, amount, fee);
+
+  let response = client.sendTokens(firstAccount.address, recipient, amount, fee)
+  console.log(response)
+  return response;
 }

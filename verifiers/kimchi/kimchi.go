9e722714105cf8cb50dc7b9b8307c194a0c1de3e
Tomas Rodriguez Dala <43424983+tomyrd@users.noreply.github.com>
2024-03-25 17:31:36 -0300

    Change kimchi library to be static (#107)

--- verifiers/kimchi/kimchi.go
+++ verifiers/kimchi/kimchi.go
@@ -2,7 +2,7 @@ package kimchi
 
 /*
 #cgo darwin LDFLAGS: -L./lib -lkimchi_verifier
-#cgo linux LDFLAGS: ${SRCDIR}/lib/libkimchi_verifier.so -ldl -lrt -lm
+#cgo linux LDFLAGS: ${SRCDIR}/lib/libkimchi_verifier.a -ldl -lrt -lm
 
 #include "lib/kimchi.h"
 */
